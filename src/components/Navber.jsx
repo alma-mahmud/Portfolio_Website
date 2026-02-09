0dd343ab4a116d1ae97268b077cfa48695f7f88d
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-02-09 09:47:33 +0600

    Navber chnage

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Navber() {
+ const Navber = ({ darkMode, toggleMode}) => {
+    const [ativeSection, setActiveSection] = useState('home')
+    const [isMemuOpen, setIsMemuOpen] =useState(false)
+    const navItem = [
+        { name: 'Home', link: '#home' },
+        { name: 'About', link: '#about' },
+        { name: 'Skills', link: '#skills' },
+        { name: 'Projects', link: '#projects' },
+        { name: 'Contact', link: '#contact' },
+    ];
+
+    const lighColors = {
+        navBg: 'bg-linear-to-br-orange-200 to-white',
+        textPrimary: 'text-gray-900',
+        textSecondary: 'text-gray-800',
+        textHover: 'text-orange-500',
+        textActive: 'text-orange-500',
+        indicator: 'from-orange-500 to-amber-500',
+        buttom: 'from-orange-500 to-amber-500',
+    };
+    
   return (
     <>
     <div>
-        
+
     </div>
     </>
   )
