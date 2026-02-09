b35cfa224e38d0eec6af34d3a676d8fb60db438b
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-02-09 09:20:19 +0600

    App  Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navber from './components/Navber'
+import AOS from 'aos'
+import 'aso/dist/aos.css'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true)
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: false,
+      offset: 100
+    });
+  })
   return (
-    <Navber></Navber>
+    <div>
+      <Navber></Navber>
+    </div>
+    
   ) 
 }
 
--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Navber() {
   return (
-    <div>Navber</div>
+    <>
+    <div>
+        
+    </div>
+    </>
   )
 }
 
