2d8ca7a0ed0df4babdfe1e97d4256d3b7c48340a
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-02-09 09:11:44 +0600

    Package Downlode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Navber from './components/Navber'
 
 function App() {
   return (
-    <div><h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
-  )
+    <Navber></Navber>
+  ) 
 }
 
 export default App
