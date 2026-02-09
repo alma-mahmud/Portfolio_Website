68d04fba56e3cc3db8a90974efb632b519ebb321
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-02-09 09:33:00 +0600

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import Navber from './components/Navber'
 import AOS from 'aos'
-import 'aso/dist/aos.css'
+import 'aos/dist/aos.css'
 
 function App() {
   const [darkMode, setDarkMode] = useState(true)
+  
   useEffect(() => {
     AOS.init({
       duration: 1000,
       once: false,
       offset: 100
     });
-  })
+    document.documentElement.classList.add('dark');
+  }, [])
+
+  const toggleDarkMode = () => {
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    document.documentElement.classList.toggle('dark');
+  };
+
   return (
-    <div>
-      <Navber></Navber>
+    <div className={
+      darkMode
+      ? 'bg-linear-to-br from-gray-900 via-[#0d182e] to-gray-900 min-h-screen'
+      : 'bg-linear-to-br from-gray-50 to-blue-50 min-h-screen'
+    }>
+      <Navber darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
     </div>
-    
   ) 
 }
 
